bdc2409acfe27799119ae9a463659028b28966bb
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-03-13 14:40:09 -0400

    added some new funtions

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI ,HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 from supabase import Client, create_client
 from db.supabase import create_supabase_client
@@ -33,13 +34,12 @@ def register_user(request: User):
         "email": email, 
         "password": password, 
     })
-    if response ['statsus'] == 400:
-        return {"message": "user regitrated succqessfully"}
-
-
+    return response
 
 @app.post('/login')
-def login_user(email: str, password: str):
+def login_user(request: User):
+    email = request.email
+    password = request.password
     data = supabase.auth.sign_in_with_password({
         "email": email, 
         "password": password
@@ -54,3 +54,35 @@ def logout_user():
 
 
 
+@app.get("/get_user")
+async def get_current_user():
+    user = await supabase.auth.get_user("access_token")
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+    return User( username=user['username'], email=user['email'])
+
+
+@app.post("/fighters")
+async def create_fighter(weight_class: str, record: str, name: str ,current_user: str = Depends(get_current_user)):
+    fighter_data = {"id": current_user, "weight_class": weight_class, "record": record, "name": name}
+    fighter = await supabase.table("Fighter").insert(fighter_data)
+    return fighter
+
+# @app.get("/fighters/{fighter_id}")
+# async def read_fighter(fighter_id: int, current_user: str = Depends(get_current_user)):
+#     fighter = await supabase.table("fighters").select("*").eq("user_id", current_user).eq("fighter_id", fighter_id).single()
+#     if not fighter:
+#         raise HTTPException(status_code=404, detail="Fighter not found")
+#     return fighter
+
+# @app.put("/fighters/{fighter_id}")
+# async def update_fighter(fighter_id: int, current_user: str = Depends(get_current_user)):
+#     fighter_data = {"user_id": current_user, "fighter_id": fighter_id}
+#     updated_fighter = await supabase.table("fighters").update(fighter_data).eq("fighter_id", fighter_id)
+#     return updated_fighter
+
+
+# @app.delete("/fighters/{fighter_id}")
+# async def delete_fighter(fighter_id: int, current_user: str = Depends(get_current_user)):
+#     deleted_fighter = await supabase.table("fighters").delete().eq("fighter_id", fighter_id)
+#     return {"message": "Fighter deleted", "fighter_id": fighter_id}
