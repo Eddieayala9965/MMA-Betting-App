375a0f41e54444b59e0eeff908de1baf95d464d3
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-03-20 16:30:56 -0400

    refactored login and registration page

--- Frontend/src/routes/Login.jsx
+++ Frontend/src/routes/Login.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router-dom";
+import { Form, Link, redirect } from "react-router-dom";
 import { useState } from "react";
 
 export const action = async ({ request }) => {
@@ -39,62 +39,148 @@ export const action = async ({ request }) => {
 };
 const Login = () => {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+    <div class="font-[sans-serif] text-[#333] bg-white">
+      <div class="grid lg:grid-cols-4 md:grid-cols-3 items-center">
+        <Form
+          class="lg:col-span-3 md:col-span-2 max-w-lg w-full p-6 mx-auto"
+          method="POST"
         >
-          <img className="h-16 w-16" src="../img/robot3.png" alt="robo" />
-          MMAChatGPT
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
-            <Form className="space-y-4 md:space-y-6" method="POST">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Email
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required
-                  />
-                </label>
-              </div>
-              <div></div>
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Password
-                  <input
-                    type="text"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
-                  />
-                </label>
-              </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          <div class="mb-16">
+            <h3 class="text-4xl font-extrabold">Sign In</h3>
+            <p class="text-sm mt-6">
+              Welcome back! Please log in to access your account and explore a
+              world of possibilities. Your journey begins here.
+            </p>
+          </div>
+          <div class="relative flex items-center">
+            <label class="text-[13px] bg-white absolute px-2 top-[-10px] left-[18px] font-semibold">
+              Email
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 outline-none"
+              required
+            />
+          </div>
+          <div class="relative flex items-center mt-10">
+            <label class="text-[13px] bg-white absolute px-2 top-[-10px] left-[18px] font-semibold">
+              Password
+            </label>
+            <input
+              name="password"
+              type="Password"
+              placeholder="Enter password"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 outline-none"
+              required
+            />
+          </div>
+          <div class="flex items-center justify-between gap-2 mt-6">
+            <div class="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+              />
+              <label for="remember-me" class="ml-3 block text-sm">
+                Remember me
+              </label>
+            </div>
+            <div>
+              <a
+                href="javascript:void(0);"
+                class="text-blue-600 text-sm hover:underline"
               >
-                Sign in
-              </button>
-            </Form>
+                Forgot Password?
+              </a>
+            </div>
+          </div>
+          <div class="mt-10">
+            <button
+              type="submit"
+              class="w-full shadow-xl py-2.5 px-4 text-sm font-semibold rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+            >
+              Sign in
+            </button>
+          </div>
+          <p class="text-sm mt-10 text-center">
+            Don't have an account
+            <Link
+              to={"/register"}
+              href="javascript:void(0);"
+              class="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap"
+            >
+              Register here
+            </Link>
+          </p>
+        </Form>
+        <div class="flex flex-col justify-center space-y-16 md:h-screen max-md:mt-16 min-h-full bg-gradient-to-r from-blue-500 to-blue-700 lg:px-8 px-4 py-4">
+          <div>
+            <h4 class="text-white text-lg font-semibold">
+              Secure Authentication
+            </h4>
+            <p class="text-[13px] text-white mt-2">
+              Log in with your registered email and password securely.
+            </p>
+          </div>
+          <div>
+            <h4 class="text-white text-lg font-semibold">Remember Me</h4>
+            <p class="text-[13px] text-white mt-2">
+              Enable the "Remember Me" option for a seamless login experience in
+              future sessions.
+            </p>
+          </div>
+          <div>
+            <h4 class="text-white text-lg font-semibold">Forgot Password?</h4>
+            <p class="text-[13px] text-white mt-2">
+              Easily recover your account by clicking on the "Forgot Password?"
+              link.
+            </p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 export default Login;
+
+// <Form className="space-y-4 md:space-y-6" method="POST">
+// <div>
+//   <label
+//     htmlFor="email"
+//     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//   >
+//     Email
+//     <input
+//       type="text"
+//       name="email"
+//       id="email"
+//       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//       placeholder="name@company.com"
+//       required
+//     />
+//   </label>
+// </div>
+// <div></div>
+// <div>
+//   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+//     Password
+//     <input
+//       type="text"
+//       name="password"
+//       id="password"
+//       placeholder="••••••••"
+//       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//       required
+//     />
+//   </label>
+// </div>
+// <button
+//   type="submit"
+//   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+// >
+//   Sign in
+// </button>
+// </Form>
