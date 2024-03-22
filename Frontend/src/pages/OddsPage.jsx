f5741c5b06550c2ce8fcf19e399a2542956dd92a
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-03-22 18:41:47 -0400

    chnaged almost made a mistake

--- Frontend/src/pages/OddsPage.jsx
+++ Frontend/src/pages/OddsPage.jsx
@@ -83,7 +83,7 @@ const OddsPage = () => {
                 className="relative grid h-auto w-full max-w-[24rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700"
               >
                 <div
-                  className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('/img/UFC.jpeg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-lg"
+                  className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('/img/UFC.jpeg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-sm"
                   style={{ backgroundSize: "cover" }}
                 >
                   <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
